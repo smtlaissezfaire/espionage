b4ef16b8b42a92df10f6193cbc304bed7a1c6da0
Scott Taylor <scott@railsnewbie.com>
2008-10-18 04:21:07 -0400

    * Remove load and find_document - it isn't used at all in the project, and   the implementation sucks

--- spec/project/include_spec.js
+++ spec/project/include_spec.js
@@ -13,54 +13,6 @@ Screw.Unit(function() {
       helper = helper_prototype.clone(helper_prototype);
     });
     
-    describe("load", function() {
-      it("should insert the js file into the document", function() {
-        helper.load("foo.js", mock_document);
-        expect(args[0]).to(equal, '<script type="text/javascript" src="foo.js"></script>');
-      });
-      
-      it("should insert the js file with the correct name", function() {
-        helper.load("boo.js", mock_document);
-        expect(args[0]).to(equal, '<script type="text/javascript" src="boo.js"></script>');
-      });
-      
-      it("should use window.document when none is provided", function() {
-        var message_received = false;
-        
-        helper.find_document = function() {
-          message_received = true;
-          return(mock_document);
-        };
-        
-        helper.load("foo.js");
-        expect(message_received).to(be_true);
-      });
-      
-      it("should provide a js extension if none is given", function() {
-        helper.load("foo", mock_document);
-        expect(args[0]).to(equal, '<script type="text/javascript" src="foo.js"></script>');
-      });
-    });
-    
-    describe("find_document", function() {
-      it("should return an empty object when the object passed is an empty object", function() {
-        var obj = {};
-        expect(helper.find_document(obj)).to(equal, obj);
-      });
-
-      it("should return the same object when the object passed in an empty object", function() {
-        var obj = {
-          foo: "bar"
-        };
-        expect(helper.find_document(obj)).to(equal, obj);
-      });
-
-      // pending: Why isn't this one passing?
-      // it("should return window.document when no object is passed", function() {
-      //   expect(helper.document()).to(equal, window.document);
-      // });
-    });
-    
     describe("object", function() {
       it("should return an object with it's prototype as the constructor", function() {
         var obj = {};
