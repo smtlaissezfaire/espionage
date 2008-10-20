78e17f0867ff481f34d8787c8b4de91a46bcf1d4
Scott Taylor <scott@railsnewbie.com>
2008-10-20 00:53:01 -0400

    * Rename spy_on to spyOn

--- spec/project/spy_spec.js
+++ spec/project/spy_spec.js
@@ -8,17 +8,17 @@ Screw.Unit(function() {
       spy = Project.Helpers.clone(Project.Spy);
     });
     
-    describe("spy_on", function() {
+    describe("spyOn", function() {
       before(function() {
         spy.reset(obj);
         obj.stub("foo");
       });
       
-      it("should call the function given to spy_on", function() {
+      it("should call the function given to spyOn", function() {
         var called = false;
         obj.foo = function() { called = true; };
         
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo();
         });
         
@@ -26,20 +26,20 @@ Screw.Unit(function() {
       });
       
       it("should report false when receiving a message", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           expect(obj.received("foo")).to(be_false);
         });
       });
       
       it("should report true when receiving a message", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo();
           expect(obj.received("foo")).to(be_true);
         });
       });
       
       it("should be able to reset the received status", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo();
         });
         
@@ -51,7 +51,7 @@ Screw.Unit(function() {
       it("should report a positive message expectation on a different method", function() {
         obj.stub("bar");
         
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.bar();
           expect(obj.received("bar")).to(be_true);
         });
@@ -60,20 +60,20 @@ Screw.Unit(function() {
       it("should not redefine a property which is not a method", function() {
         obj.bar = 7;
         
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           expect(obj.bar).to(equal, 7);
         });
       });
       
       it("should be false when the wrong method is called", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo();
           expect(obj.received("bar")).to(be_false);
         });
       });
       
       it("should return false for a method which is not defined", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           expect(obj.received("baz")).to(be_false);
         });
       });
@@ -87,7 +87,7 @@ Screw.Unit(function() {
           }
         };
         
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           other_obj.foo(obj);
           expect(obj.received("bar")).to(be_true);
         });
@@ -101,21 +101,21 @@ Screw.Unit(function() {
       });
       
       it("should have an empty array of arguments when none are provided", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo();
           expect(obj.argumentsFor("foo")).to(equal, [[]]);
         });
       });
       
       it("should have one argument when given", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo("bar");
           expect(obj.argumentsFor("foo")).to(equal, [["bar"]]);
         });
       });
       
       it("should have two arguments when given", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo(1, 2);
           expect(obj.argumentsFor("foo")).to(equal, [[1,2]]);
         });
@@ -124,14 +124,14 @@ Screw.Unit(function() {
       it("should use the correct method", function() {
         obj.stub("bar");
         
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.bar(1, 2);
           expect(obj.argumentsFor("bar")).to(equal, [[1,2]]);
         });
       });
       
       it("should return undefined if the method was never called", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           expect(obj.argumentsFor("bar")).to(equal, undefined);
         });
       });
@@ -140,14 +140,14 @@ Screw.Unit(function() {
         var an_object = {};
         an_object.stub("foo");
         
-        spy.spy_on(an_object, function() {
+        spy.spyOn(an_object, function() {
           an_object.foo([]);
           expect(an_object.argumentsFor("foo")).to(equal, [[[]]]);
         });
       });
       
       it("should return two pairs of arguments if called twice", function() {
-        spy.spy_on(obj, function() {
+        spy.spyOn(obj, function() {
           obj.foo();
           obj.foo();
           expect(obj.argumentsFor("foo")).to(equal, [[], []]);
