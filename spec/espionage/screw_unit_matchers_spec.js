4f4c4977f3f58809ed1a85909303b93ff193f4fe
Scott Taylor <scott@railsnewbie.com>
2008-10-20 22:16:16 -0400

    * Add #have_received matcher

--- spec/espionage/screw_unit_matchers_spec.js
+++ spec/espionage/screw_unit_matchers_spec.js
@@ -90,6 +90,85 @@ Screw.Unit(function() {
           });
         });
       });
+
+      describe("#have_received", function() {
+        var obj = {};
+        
+        before(function() {
+          obj = {};
+          obj.stub("foo");
+        });
+        
+        describe("internals", function() {
+          var matcher;
+          
+          before(function() {
+            matcher = helper.clone(Espionage.ScrewUnit.Matchers.Spy).have_received;
+          });
+          
+          describe("matching", function() {
+            it("should match when the argument has been called", function() {
+              spy.spyOn(obj, function() {
+                obj.foo();
+                expect(matcher.match("foo", obj)).to(be_true);
+              });
+            });
+            
+            it("should not match when the method hasn't been called", function() {
+              spy.spyOn(obj, function() {
+                expect(matcher.match("foo", obj)).to(be_false);
+              });
+            });
+          });
+          
+          describe("failure_message", function() {
+            describe("positive failure message", function() {
+              it("should have the correct failure message", function() {
+                var message = "Expected 'foo' to be called";
+        
+                spy.spyOn(obj, function() {
+                  expect(matcher.failure_message("foo", obj, false)).to(equal, message);
+                });
+              });
+        
+              it("should use the proper message name", function() {
+                spy.spyOn(obj, function() {
+                  expect(matcher.failure_message("bar", obj, false)).to(match, /bar/);
+                });
+              });
+            });
+            
+            describe("negative failure_message", function() {
+              it("should have the message expectation", function() {
+                var message = "Expected 'foo' to not be called";
+        
+                spy.spyOn(obj, function() {
+                  expect(matcher.failure_message("foo", obj, true)).to(equal, message);
+                });
+              });
+        
+              it("should use the proper message name", function() {
+                spy.spyOn(obj, function() {
+                  expect(matcher.failure_message("bar", obj, true)).to(match, /bar/);
+                });
+              });
+            });
+          });
+        });
+
+        it("should pass #have_received when it's been invoked", function() {
+          spy.spyOn(obj, function() {
+            obj.foo();
+            expect(obj).to(have_received, "foo");
+          });
+        });
+        
+        it("should fail #have_received when it hasn't been invoked at all", function() {
+          spy.spyOn(obj, function() {
+            expect(obj).to_not(have_received, "foo");
+          });
+        });
+      });
     });
   });
 });
