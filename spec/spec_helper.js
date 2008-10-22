5781f58760d20a9b1f31a0604b327ecfa0471db6
Scott Taylor <scott@railsnewbie.com>
2008-10-22 00:26:59 -0400

    * Cleanup spec loading

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,32 +1,55 @@
-function cold_start_load(jsFile) {
-  document.write('<script type="text/javascript" src="' + jsFile + '"></script>');
+if(!typeof(Espionage) === "object") {
+  Espionage = {};
 }
 
-load = cold_start_load;
-
-function load_screw_unit() {
-  var project_root = "..";
-  var screw_unit_path = project_root + "/vendor/screw_unit/lib";
+Espionage = {};
+Espionage.SpecHelpers = function() {
+  function cold_start_load(jsFile) {
+    document.write('<script type="text/javascript" src="' + jsFile + '"></script>');
+  }
   
-  load(screw_unit_path + "/jquery-1.2.3.js");
-  load(screw_unit_path + "/jquery.fn.js");
-  load(screw_unit_path + "/jquery.print.js");
-  load(screw_unit_path + "/screw.builder.js");
-  load(screw_unit_path + "/screw.matchers.js");
-  load(screw_unit_path + "/screw.events.js");
-  load(screw_unit_path + "/screw.behaviors.js");
-}
+  var load_file = cold_start_load;
 
-function load_project() {
-  load("../lib/espionage/helpers.js");
-  load("../lib/espionage/stub.js");
-  load("../lib/espionage/spy.js");
-  load("../lib/espionage/screw_unit/matchers.js");
-}
+  function load_screw_unit() {
+    var project_root = "..";
+    var screw_unit_path = project_root + "/vendor/screw_unit/lib";
 
-function load_specs() {
-  load("espionage/helpers_spec.js");
-  load("espionage/stub_spec.js");
-  load("espionage/spy_spec.js");
-  load("espionage/screw_unit_matchers_spec.js");
-}
+    load_file(screw_unit_path + "/jquery-1.2.3.js");
+    load_file(screw_unit_path + "/jquery.fn.js");
+    load_file(screw_unit_path + "/jquery.print.js");
+    load_file(screw_unit_path + "/screw.builder.js");
+    load_file(screw_unit_path + "/screw.matchers.js");
+    load_file(screw_unit_path + "/screw.events.js");
+    load_file(screw_unit_path + "/screw.behaviors.js");
+  }
+
+  function load_project() {
+    var lib_path = "../lib";
+
+    load_file(lib_path + "/espionage/helpers.js");
+    load_file(lib_path + "/espionage/stub.js");
+    load_file(lib_path + "/espionage/spy.js");
+    load_file(lib_path + "/espionage/screw_unit/matchers.js");
+  }
+
+  function load_specs() {
+    var spec_path = "espionage";
+
+    load_file(spec_path + "/helpers_spec.js");
+    load_file(spec_path + "/stub_spec.js");
+    load_file(spec_path + "/spy_spec.js");
+    load_file(spec_path + "/screw_unit_matchers_spec.js");
+  }
+  
+  var loadProject = function() {
+    load_screw_unit();
+    load_project();
+    load_specs();
+  };
+  
+  return {
+    loadProject: loadProject
+  };
+}();
+
+Espionage.SpecHelpers.loadProject();
