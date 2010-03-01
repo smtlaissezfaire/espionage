07cb6f63a4ba911e87654a299554c13ccfb58031
Scott Taylor <scott@railsnewbie.com>
2010-03-01 04:09:11 -0500

    Fix issues with restoring stubs on properties that were === undefined before stubbing

--- spec/espionage/global_spec.js
+++ spec/espionage/global_spec.js
@@ -1,6 +1,10 @@
 describe("Espionage", function() {
   describe("global methods", function() {
     describe("stub", function() {
+      before(function() {
+        Espionage.Stub.teardown();
+      });
+
       it("should stub a value on an object", function() {
         var obj = {};
         obj.stub("foo", 1);
@@ -11,6 +15,37 @@ describe("Espionage", function() {
         var obj = {};
         obj.stub("foo");
         obj.foo().should.equal(undefined);
+      });
+
+      it("should delete the property of an object which wasn't defined after teardown", function() {
+        var obj = {};
+
+        obj.stub("foo");
+        Espionage.Stub.teardown();
+        obj.hasOwnProperty("foo").should.be(false);
+      });
+
+      it("should not delete the property if set to undefined before stubbing", function() {
+        var obj = {};
+
+        obj.foo = undefined;
+        obj.stub("foo");
+        Espionage.Stub.teardown();
+
+        obj.hasOwnProperty("foo").should.be(true);
+      });
+
+      it("should restore a prototype property appropriately", function() {
+        var proto = {
+          foo: "bar"
+        };
+
+        var obj = Espionage.Helpers.clone(proto);
+        obj.stub("foo");
+
+        Espionage.Stub.teardown();
+
+        obj.foo.should.equal("bar");
       });
     });
   });
